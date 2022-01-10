f4f870806a76564372399ef6c2ba101120a4dc5d
Ohgochi <toyoaki2@gmail.com>
2022-01-10 12:00:20 +0900

    BUGfix: Sensor Info's subtitle

--- app/src/main/java/com/example/camera2probeK/ReadSensorInfo.kt
+++ app/src/main/java/com/example/camera2probeK/ReadSensorInfo.kt
@@ -27,14 +27,14 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (sensorPixelArraySize != null)
             sensorPixelArraySizeTxt =
                     sensorPixelArraySize.width.toString() + " x " + sensorPixelArraySize.height.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorPixelArraySizeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorPixelArraySizeTxt, NONE))
 
         subtitle = "Sensor Active Array Size: "
         val sensorInfoActiveArraySize = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE)
         var sensorInfoActiveArraySizeTxt = "Could not get"
         if (sensorInfoActiveArraySize != null)
             sensorInfoActiveArraySizeTxt = sensorInfoActiveArraySize.toShortString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoActiveArraySizeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoActiveArraySizeTxt, NONE))
 
         subtitle = "Sensor Active Array Size Max Res: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -43,7 +43,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE_MAXIMUM_RESOLUTION)
             if (sensorInfoActiveArraySizeMaxRes != null)
                 sensorInfoActiveArraySizeMaxResTxt = sensorInfoActiveArraySizeMaxRes.toShortString()
-            specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoActiveArraySizeMaxResTxt, NONE))
+            specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoActiveArraySizeMaxResTxt, NONE))
         }
 
         subtitle = "Sensor Array Size Max Res: "
@@ -54,7 +54,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             if (sensorInfoPixelArraySizeMax != null)
                 sensorInfoPixelArraySizeMaxTxt =
                         sensorInfoPixelArraySizeMax.width.toString() + " x " + sensorInfoPixelArraySizeMax.height.toString()
-            specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPixelArraySizeMaxTxt, NONE))
+            specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoPixelArraySizeMaxTxt, NONE))
         }
 
         subtitle = "Sensor Pre Collective Array Size: "
@@ -63,7 +63,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         var sensorInfoPreCorrectedActiveArraySizeTxt = "Could not get"
         if (sensorInfoPreCorrectedActiveArraySize != null)
             sensorInfoPreCorrectedActiveArraySizeTxt = sensorInfoPreCorrectedActiveArraySize.toShortString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPreCorrectedActiveArraySizeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoPreCorrectedActiveArraySizeTxt, NONE))
 
         subtitle = "Sensor Pre Collective Array Size Max Res: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -72,7 +72,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             var sensorInfoPreCorrectedActiveArraySizeMaxTxt = "Could not get"
             if (sensorInfoPreCorrectedActiveArraySizeMax != null)
                 sensorInfoPreCorrectedActiveArraySizeMaxTxt = sensorInfoPreCorrectedActiveArraySizeMax.toShortString()
-            specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPreCorrectedActiveArraySizeMaxTxt, NONE))
+            specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoPreCorrectedActiveArraySizeMaxTxt, NONE))
         }
 
         subtitle = "Sensor Physical Size: "
@@ -81,7 +81,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (sensorInfoPixelPhysicalSize != null)
             sensorInfoPixelPhysicalSizeTxt =
                     sensorInfoPixelPhysicalSize.width.toString() + " x " + sensorInfoPixelPhysicalSize.height.toString() + " mm"
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPixelPhysicalSizeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoPixelPhysicalSizeTxt, NONE))
 
         subtitle = "Sensor Binning Factor: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -91,7 +91,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             if (sensorInfoBinningFactor != null)
                 sensorInfoBinningFactorTxt =
                         sensorInfoBinningFactor.width.toString() + " x " + sensorInfoBinningFactor.height.toString()
-            specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoBinningFactorTxt, NONE))
+            specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoBinningFactorTxt, NONE))
         }
 
         subtitle = "Sensor Exposure Time Range: "
@@ -101,14 +101,14 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (sensorInfoExposureTimeRange != null)
             sensorInfoExposureTimeRangeTxt =
                     sensorInfoExposureTimeRange.lower.toString() + " to " + sensorInfoExposureTimeRange.upper.toString() + " nanoseconds"
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoExposureTimeRangeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoExposureTimeRangeTxt, NONE))
 
         subtitle = "Sensor Orientation: "
         val sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
         var sensorOrientationTxt = "Could not get"
         if (sensorOrientation != null)
             sensorOrientationTxt = sensorOrientation.toString() + " deg"
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorOrientationTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorOrientationTxt, NONE))
 
         subtitle = "Sensor Sensitivity ISO Range: "
         val sensorInfoSensitivityRange = characteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE)
@@ -116,13 +116,13 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (sensorInfoSensitivityRange != null)
             sensorInfoSensitivityRangeTxt =
                     sensorInfoSensitivityRange.lower.toString() + " to " + sensorInfoSensitivityRange.upper.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoSensitivityRangeTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorInfoSensitivityRangeTxt, NONE))
 
         subtitle = "Sensor White Level: "
         val sensorWhiteLevel = characteristics.get(CameraCharacteristics.SENSOR_INFO_WHITE_LEVEL)
         var sensorWhiteLevelTxt = "not supported"
         if (sensorWhiteLevel != null) sensorWhiteLevelTxt = sensorWhiteLevel.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorWhiteLevelTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorWhiteLevelTxt, NONE))
 
         subtitle = "Sensor Black Level Pattern: "
         val sensorBlackLevelPattern = characteristics.get(CameraCharacteristics.SENSOR_BLACK_LEVEL_PATTERN)
@@ -133,13 +133,13 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorBlackLevelPattern.getOffsetForIndex(0,1) + "],[" +
                     sensorBlackLevelPattern.getOffsetForIndex(1,0) + "," +
                     sensorBlackLevelPattern.getOffsetForIndex(1,1) + "]"
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorBlackLevelPatternTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorBlackLevelPatternTxt, NONE))
 
         subtitle = "Sensor Max Analog Sensitivity: "
         val sensorMaxAnalogSensitivity = characteristics.get(CameraCharacteristics.SENSOR_MAX_ANALOG_SENSITIVITY)
         var sensorMaxAnalogSensitivityTxt = "not supported"
         if (sensorMaxAnalogSensitivity != null) sensorMaxAnalogSensitivityTxt = sensorMaxAnalogSensitivity.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorMaxAnalogSensitivityTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + sensorMaxAnalogSensitivityTxt, NONE))
 
         subtitle = "Sensor Sensitivity Timestamp Source: "
         val timestampSource = characteristics.get(CameraCharacteristics.SENSOR_INFO_TIMESTAMP_SOURCE)
@@ -147,7 +147,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (timestampSource != null) {
             timestampSourceTxt = GetOverviewTimestampSource().get(timestampSource)
         }
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + timestampSourceTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + timestampSourceTxt, NONE))
 
         subtitle = "Color filters on sensor: "
         val colorFilterArrangement = characteristics.get(CameraCharacteristics.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT)
@@ -155,7 +155,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         if (colorFilterArrangement != null) {
             colorFilterArrangementTxt = GetOverviewColorFilterArrangement().get(colorFilterArrangement)
         }
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + colorFilterArrangementTxt, NONE))
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + colorFilterArrangementTxt, NONE))
 
         subtitle = "Sensor Calibration Transform 1 : "
         val calibrationTransform1 = characteristics.get(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM1)
