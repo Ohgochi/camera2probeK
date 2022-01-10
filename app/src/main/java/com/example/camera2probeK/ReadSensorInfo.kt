b986a7ca27feeaa897d0b37d21e9e16334236859
Ohgochi <toyoaki2@gmail.com>
2022-01-10 10:09:01 +0900

    BUGfix SENSOR_OPTICAL_BLACK_REGIONS Arrangement of display related to sensor related information

--- app/src/main/java/com/example/camera2probeK/ReadSensorInfo.kt
+++ app/src/main/java/com/example/camera2probeK/ReadSensorInfo.kt
@@ -21,7 +21,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         specs.add(CameraSpecResult(KEY_BRAKE, "", NONE))
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title, NONE))
 
-        title = "Sensor Array Size: "
+        var subtitle = "Sensor Array Size: "
         val sensorPixelArraySize = characteristics.get(CameraCharacteristics.SENSOR_INFO_PIXEL_ARRAY_SIZE)
         var sensorPixelArraySizeTxt = "Could not get"
         if (sensorPixelArraySize != null)
@@ -29,14 +29,14 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorPixelArraySize.width.toString() + " x " + sensorPixelArraySize.height.toString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorPixelArraySizeTxt, NONE))
 
-        title = "Sensor Active Array Size: "
+        subtitle = "Sensor Active Array Size: "
         val sensorInfoActiveArraySize = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE)
         var sensorInfoActiveArraySizeTxt = "Could not get"
         if (sensorInfoActiveArraySize != null)
             sensorInfoActiveArraySizeTxt = sensorInfoActiveArraySize.toShortString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoActiveArraySizeTxt, NONE))
 
-        title = "Sensor Active Array Size Max Res: "
+        subtitle = "Sensor Active Array Size Max Res: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             var sensorInfoActiveArraySizeMaxResTxt = "Not supported"
             val sensorInfoActiveArraySizeMaxRes =
@@ -46,7 +46,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoActiveArraySizeMaxResTxt, NONE))
         }
 
-        title = "Sensor Array Size Max Res: "
+        subtitle = "Sensor Array Size Max Res: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val sensorInfoPixelArraySizeMax =
                     characteristics.get(CameraCharacteristics.SENSOR_INFO_PIXEL_ARRAY_SIZE_MAXIMUM_RESOLUTION)
@@ -57,7 +57,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPixelArraySizeMaxTxt, NONE))
         }
 
-        title = "Sensor Pre Collective Array Size: "
+        subtitle = "Sensor Pre Collective Array Size: "
         val sensorInfoPreCorrectedActiveArraySize =
                 characteristics.get(CameraCharacteristics.SENSOR_INFO_PRE_CORRECTION_ACTIVE_ARRAY_SIZE)
         var sensorInfoPreCorrectedActiveArraySizeTxt = "Could not get"
@@ -65,7 +65,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             sensorInfoPreCorrectedActiveArraySizeTxt = sensorInfoPreCorrectedActiveArraySize.toShortString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPreCorrectedActiveArraySizeTxt, NONE))
 
-        title = "Sensor Pre Collective Array Size Max Res: "
+        subtitle = "Sensor Pre Collective Array Size Max Res: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val sensorInfoPreCorrectedActiveArraySizeMax =
                     characteristics.get(CameraCharacteristics.SENSOR_INFO_PRE_CORRECTION_ACTIVE_ARRAY_SIZE_MAXIMUM_RESOLUTION)
@@ -75,7 +75,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPreCorrectedActiveArraySizeMaxTxt, NONE))
         }
 
-        title = "Sensor Physical Size: "
+        subtitle = "Sensor Physical Size: "
         val sensorInfoPixelPhysicalSize = characteristics.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)
         var sensorInfoPixelPhysicalSizeTxt = "Could not get"
         if (sensorInfoPixelPhysicalSize != null)
@@ -83,7 +83,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorInfoPixelPhysicalSize.width.toString() + " x " + sensorInfoPixelPhysicalSize.height.toString() + " mm"
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoPixelPhysicalSizeTxt, NONE))
 
-        title = "Sensor Binning Factor: "
+        subtitle = "Sensor Binning Factor: "
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val sensorInfoBinningFactor =
                     characteristics.get(CameraCharacteristics.SENSOR_INFO_BINNING_FACTOR)
@@ -94,7 +94,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoBinningFactorTxt, NONE))
         }
 
-        title = "Sensor Exposure Time Range: "
+        subtitle = "Sensor Exposure Time Range: "
         val sensorInfoExposureTimeRange =
                 characteristics.get(CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE)
         var sensorInfoExposureTimeRangeTxt = "CNot supported"
@@ -103,14 +103,14 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorInfoExposureTimeRange.lower.toString() + " to " + sensorInfoExposureTimeRange.upper.toString() + " nanoseconds"
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoExposureTimeRangeTxt, NONE))
 
-        title = "Sensor Orientation: "
+        subtitle = "Sensor Orientation: "
         val sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
         var sensorOrientationTxt = "Could not get"
         if (sensorOrientation != null)
             sensorOrientationTxt = sensorOrientation.toString() + " deg"
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorOrientationTxt, NONE))
 
-        title = "Sensor Sensitivity ISO Range: "
+        subtitle = "Sensor Sensitivity ISO Range: "
         val sensorInfoSensitivityRange = characteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE)
         var sensorInfoSensitivityRangeTxt = "ISO 12232:2006 not supported"
         if (sensorInfoSensitivityRange != null)
@@ -118,13 +118,13 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorInfoSensitivityRange.lower.toString() + " to " + sensorInfoSensitivityRange.upper.toString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorInfoSensitivityRangeTxt, NONE))
 
-        title = "Sensor White Level: "
+        subtitle = "Sensor White Level: "
         val sensorWhiteLevel = characteristics.get(CameraCharacteristics.SENSOR_INFO_WHITE_LEVEL)
         var sensorWhiteLevelTxt = "not supported"
         if (sensorWhiteLevel != null) sensorWhiteLevelTxt = sensorWhiteLevel.toString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorWhiteLevelTxt, NONE))
 
-        title = "Sensor Black Level Pattern: "
+        subtitle = "Sensor Black Level Pattern: "
         val sensorBlackLevelPattern = characteristics.get(CameraCharacteristics.SENSOR_BLACK_LEVEL_PATTERN)
         var sensorBlackLevelPatternTxt = "Could not get"
         if (sensorBlackLevelPattern != null)
@@ -135,13 +135,13 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
                     sensorBlackLevelPattern.getOffsetForIndex(1,1) + "]"
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorBlackLevelPatternTxt, NONE))
 
-        title = "Sensor Max Analog Sensitivity: "
+        subtitle = "Sensor Max Analog Sensitivity: "
         val sensorMaxAnalogSensitivity = characteristics.get(CameraCharacteristics.SENSOR_MAX_ANALOG_SENSITIVITY)
         var sensorMaxAnalogSensitivityTxt = "not supported"
         if (sensorMaxAnalogSensitivity != null) sensorMaxAnalogSensitivityTxt = sensorMaxAnalogSensitivity.toString()
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + sensorMaxAnalogSensitivityTxt, NONE))
 
-        title = "Sensor Sensitivity Timestamp Source: "
+        subtitle = "Sensor Sensitivity Timestamp Source: "
         val timestampSource = characteristics.get(CameraCharacteristics.SENSOR_INFO_TIMESTAMP_SOURCE)
         var timestampSourceTxt = "Could not get"
         if (timestampSource != null) {
@@ -149,7 +149,7 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         }
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + timestampSourceTxt, NONE))
 
-        title = "Color filters on sensor: "
+        subtitle = "Color filters on sensor: "
         val colorFilterArrangement = characteristics.get(CameraCharacteristics.SENSOR_INFO_COLOR_FILTER_ARRANGEMENT)
         var colorFilterArrangementTxt = "Could not get"
         if (colorFilterArrangement != null) {
@@ -157,7 +157,53 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
         }
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title + colorFilterArrangementTxt, NONE))
 
-        title = "Sensor Test Pattern Modes: "
+        subtitle = "Sensor Calibration Transform 1 : "
+        val calibrationTransform1 = characteristics.get(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM1)
+        var calibrationTransform1Txt = "Could not get"
+        // TODO I couldn't check if the value I got with toString() was correct
+        if (calibrationTransform1 != null)
+            calibrationTransform1Txt = calibrationTransform1.toString()
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + calibrationTransform1Txt, NONE))
+
+        subtitle = "Sensor Calibration Transform with 2nd ref illuminant: "
+        val calibrationTransform2 = characteristics.get(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM2)
+        var calibrationTransform2Txt = "Could not get"
+        // TODO I couldn't check if the value I got with toString() was correct
+        if (calibrationTransform2 != null)
+            calibrationTransform2Txt = calibrationTransform2.toString()
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + calibrationTransform2Txt, NONE))
+
+        subtitle = "Color Transform (D50 Whitepoint) 1: "
+        val forwordMatrix1 = characteristics.get(CameraCharacteristics.SENSOR_FORWARD_MATRIX1)
+        var forwordMatrix1Txt = "Could not get"
+        // TODO I couldn't check if the value I got with toString() was correct
+        if (forwordMatrix1 != null)
+            forwordMatrix1Txt = forwordMatrix1.toString()
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + forwordMatrix1Txt, NONE))
+
+        subtitle = "Color Transform (D50 Whitepoint) with 2nd ref illuminant: "
+        val forwordMatrix2 = characteristics.get(CameraCharacteristics.SENSOR_FORWARD_MATRIX2)
+        var forwordMatrix2Txt = "Could not get"
+        // TODO I couldn't check if the value I got with toString() was correct
+        if (forwordMatrix2 != null)
+            forwordMatrix2Txt = forwordMatrix2.toString()
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + forwordMatrix2Txt, NONE))
+
+        subtitle = "Reference Illuminant 1: "
+        val referenceIlluminate1 = characteristics.get(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT1)
+        var referenceIlluminate1Txt = "Could not get"
+        if (referenceIlluminate1 != null)
+            referenceIlluminate1Txt = GetOverviewReferenceIlluminant1().get(referenceIlluminate1)
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + referenceIlluminate1Txt, NONE))
+
+        subtitle = "Reference Illuminant 2: "
+        val referenceIlluminate2 = characteristics.get(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT2)
+        var referenceIlluminate2Txt = "Could not get"
+        if (referenceIlluminate2 != null)
+            referenceIlluminate2Txt = referenceIlluminate2.toString(16)
+        specs.add(CameraSpecResult(KEY_INDENT_PARA, subtitle + referenceIlluminate2Txt, NONE))
+
+        title = "Sensor Test Pattern Modes "
         val sensorTestPatternModes = characteristics.get(CameraCharacteristics.SENSOR_AVAILABLE_TEST_PATTERN_MODES)
         if (sensorTestPatternModes != null) {
             specs.addAll(getOverviewList(title, GetOverviewTestPatternModes().get(), sensorTestPatternModes))
@@ -166,67 +212,17 @@ class ReadSensorInfo(characteristics: CameraCharacteristics) : CameraSpecs(chara
             specs.add(CameraSpecResult(KEY_NEWLINE, title + sensorTestPatternModesTxt, NONE))
         }
 
-        title = "Sensor Optical Black Regions: "
+        title = "Sensor Optical Black Regions"
         specs.add(CameraSpecResult(KEY_INDENT_PARA, title, NONE))
         val blackRegions = characteristics.get(CameraCharacteristics.SENSOR_OPTICAL_BLACK_REGIONS)
-        var blackRedionsTxt = "No support"
+        var blackRedionsTxt = "Not supported"
         if (blackRegions != null) {
             blackRegions.forEachIndexed { index, rect ->
                 blackRedionsTxt = "[" + index.toString() + "] " + rect.toShortString()
-                specs.add(CameraSpecResult(KEY_INDENT_PARA, rect.toShortString(), NONE))
+                specs.add(CameraSpecResult(KEY_INDENT_PARA, blackRedionsTxt, NONE))
             }
         } else
-            specs.add(CameraSpecResult(KEY_INDENT_PARA, title, NONE))
-
-        title = "Sensor Calibration Transform 1 : "
-        specs.add(CameraSpecResult(KEY_RESET, "", NONE))
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title, NONE))
-        val calibrationTransform1 = characteristics.get(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM1)
-        var calibrationTransform1Txt = "Could not get"
-        // TODO I couldn't check if the value I got with toString() was correct
-        if (calibrationTransform1 != null)
-            calibrationTransform1Txt = calibrationTransform1.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, calibrationTransform1Txt, NONE))
-
-        title = "Sensor Calibration Transform with 2nd ref illuminant: "
-        specs.add(CameraSpecResult(KEY_RESET, "", NONE))
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title, NONE))
-        val calibrationTransform2 = characteristics.get(CameraCharacteristics.SENSOR_CALIBRATION_TRANSFORM2)
-        var calibrationTransform2Txt = "Could not get"
-        // TODO I couldn't check if the value I got with toString() was correct
-        if (calibrationTransform2 != null)
-            calibrationTransform2Txt = calibrationTransform2.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, calibrationTransform2Txt, NONE))
-
-        title = "Color Transform (D50 Whitepoint) 1: "
-        val forwordMatrix1 = characteristics.get(CameraCharacteristics.SENSOR_FORWARD_MATRIX1)
-        var forwordMatrix1Txt = "Could not get"
-        // TODO I couldn't check if the value I got with toString() was correct
-        if (forwordMatrix1 != null)
-            forwordMatrix1Txt = forwordMatrix1.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + forwordMatrix1Txt, NONE))
-
-        title = "Color Transform (D50 Whitepoint) with 2nd ref illuminant: "
-        val forwordMatrix2 = characteristics.get(CameraCharacteristics.SENSOR_FORWARD_MATRIX2)
-        var forwordMatrix2Txt = "Could not get"
-        // TODO I couldn't check if the value I got with toString() was correct
-        if (forwordMatrix2 != null)
-            forwordMatrix2Txt = forwordMatrix2.toString()
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + forwordMatrix2Txt, NONE))
-
-        title = "Reference Illuminant 1: "
-        val referenceIlluminate1 = characteristics.get(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT1)
-        var referenceIlluminate1Txt = "Could not get"
-        if (referenceIlluminate1 != null)
-            referenceIlluminate1Txt = GetOverviewReferenceIlluminant1().get(referenceIlluminate1)
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + referenceIlluminate1Txt, NONE))
-
-        title = "Reference Illuminant 2: "
-        val referenceIlluminate2 = characteristics.get(CameraCharacteristics.SENSOR_REFERENCE_ILLUMINANT2)
-        var referenceIlluminate2Txt = "Could not get"
-        if (referenceIlluminate2 != null)
-            referenceIlluminate2Txt = referenceIlluminate2.toString(16)
-        specs.add(CameraSpecResult(KEY_INDENT_PARA, title + referenceIlluminate2Txt, NONE))
+            specs.add(CameraSpecResult(KEY_INDENT_PARA, blackRedionsTxt, NONE))
 
         return specs
     }
